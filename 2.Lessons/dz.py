8eec7c6a943f7f514f632ef493e6c33ca9ea63b6
Tirincanti Y <kruzhanovska010@gmail.com>
2022-04-12 15:25:45 +0200

    lesson 2

---
+++ 2.Lessons/dz.py
@@ -0,0 +1,17 @@
+a = input("Введіть  перше число: ")
+b = input("Введіть  друге число: ")
+c = input("Введіть  дію: ")
+if c == "+":
+    print("a + b =",str(int(a) + int(b)))
+elif c == "-":
+    print("a - b =",str(int(a) - int(b)))
+elif c == "*":
+    print("a * b =",str(int(a) * int(b)))
+elif c == "/":
+    if b == "0":
+        print("error")
+    else
+    print("a / b =",str(int(a) / int(b)))
+
+
+
---
+++ 2.Lessons/start.py
@@ -0,0 +1,12 @@
+print("Hello World!")
+
+first = 23
+print(first)
+second = "Number"
+print(second)
+
+print(first,second)
+
+a = 10 + 5
+b = a + 20
+c = ( a + b) * 2
